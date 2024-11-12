f960df8e9272d77fc08dbb0ddba1f5e99289baa6
Yousef Bakhsh <85824409+ba5shy@users.noreply.github.com>
2024-11-12 19:53:33 +0300

    Added updated files

---
+++ src/Server.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable{
+
+    private int port_number;
+    private String host_username;
+    private String client_usernamel;
+    private ServerSocket server_socket;
+    private Socket client_socket;
+    
+
+    public Server(int port_number, String host_username){
+        this.port_number = port_number;
+        this.host_username = host_username;
+    }
+
+    public void run() {
+        // server implementation
+        try{
+            while(true){
+                server_socket = new ServerSocket(this.port_number);
+                System.out.println("Server Started");
+                
+                client_socket = server_socket.accept();
+
+                System.out.println("Client Connected");
+                
+            }
+            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
